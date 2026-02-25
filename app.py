c28b4df64b84cce798ee15ee9cdd08233767499b
KARTIK KAISTHA <118113163+kartikcoder18@users.noreply.github.com>
2026-02-25 10:45:20 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if _name_ == "_main_":
+    x = 10
+    y = 5
+
+    print("First number:", x)
+    print("Second number:", y)
+
+    print("Addition:", add(x, y))
+    print("Subtraction:", subtract(x, y))
